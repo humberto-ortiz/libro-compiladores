526afa7760642f22658ecafd1ab45983d8f42c0a
Humberto Ortiz-Zuazaga <humberto.ortiz@upr.edu>
2020-12-11 21:53:06 -0400

    Added lalrpop parser and lexer

--- src/inc/src/main.rs
+++ src/inc/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate lalrpop_util;
+
+lalrpop_mod!(pub nums); // synthesized by LALRPOP
+
+#[test]
+fn numbers() {
+  assert!(nums::NumParser::new().parse("22").is_ok());
+  assert!(nums::NumParser::new().parse("123").is_ok());
+  assert!(nums::NumParser::new().parse("-22)").is_err());
+}
+
+use std::fs;
+use std::env;
+
+fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
+    let args: Vec<String> = env::args().collect();
+
+    let filename = &args[1];
+
+    let foo = fs::read_to_string(filename)?;
+    let prog = nums::NumParser::new().parse(&foo);
+    println!("{:?}", prog.unwrap());
+    Ok(())
 }
